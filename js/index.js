59b02ecf8fc2e5c40a15d4d17e83c1aff3f9dd5b
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-04 22:53:35 +0800

    card-dropdownhover

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ function toggleMenu() {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
 }
+function showDescription(card) {
+  const description = card.querySelector(".--worktitle p");
+  description.style.visibility = "visible";
+  description.style.opacity = "1";
+}
+
+function hideDescription(card) {
+  const description = card.querySelector(".--worktitle p");
+  description.style.visibility = "hidden";
+  description.style.opacity = "0";
+}
