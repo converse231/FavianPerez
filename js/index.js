7b0a5862c81440d4217555a42f966c1736a7f1ab
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-04 23:11:10 +0800

    minor changes css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
-function toggleMenu() {
-  const navLinks = document.getElementById("navLinks");
-  const hamburger = document.querySelector(".hamburger");
-
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.getElementById("navLinks");
+const toggleMenu = () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
-}
-function showDescription(card) {
+};
+const showDescription = (card) => {
   const description = card.querySelector(".--worktitle p");
   description.style.visibility = "visible";
   description.style.opacity = "1";
-}
+};
 
-function hideDescription(card) {
+const hideDescription = (card) => {
   const description = card.querySelector(".--worktitle p");
   description.style.visibility = "hidden";
   description.style.opacity = "0";
-}
+};
+
+hamburger.addEventListener("click", toggleMenu);
