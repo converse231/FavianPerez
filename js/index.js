45ea7966754111cd147baa5502a9483ae2239ccb
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-04 23:17:34 +0800

    js file changes

--- js/index.js
+++ js/index.js
@@ -5,4 +5,27 @@ const toggleMenu = () => {
   hamburger.classList.toggle("active");
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+  const workoutCards = document.querySelectorAll(".--workoutcards");
+
+  const removeClickedClass = () => {
+    workoutCards.forEach((card) => card.classList.remove("clicked"));
+  };
+
+  document.addEventListener("click", (event) => {
+    const clickedCard = event.target.closest(".--workoutcards");
+    if (!clickedCard) {
+      removeClickedClass();
+    } else {
+      workoutCards.forEach((card) => {
+        if (card === clickedCard) {
+          card.classList.toggle("clicked");
+        } else {
+          card.classList.remove("clicked");
+        }
+      });
+    }
+  });
+});
+
 hamburger.addEventListener("click", toggleMenu);
