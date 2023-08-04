75286dcadf5e0fefcea84093f0fa124196cf3be3
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-04 23:12:07 +0800

    changes

--- js/index.js
+++ js/index.js
@@ -4,16 +4,5 @@ const toggleMenu = () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
 };
-const showDescription = (card) => {
-  const description = card.querySelector(".--worktitle p");
-  description.style.visibility = "visible";
-  description.style.opacity = "1";
-};
-
-const hideDescription = (card) => {
-  const description = card.querySelector(".--worktitle p");
-  description.style.visibility = "hidden";
-  description.style.opacity = "0";
-};
 
 hamburger.addEventListener("click", toggleMenu);
